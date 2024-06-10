6d16f48f68391efcc82c703ec214a28ed06e77b1
Ishan <345ishaan@gmail.com>
2024-06-10 01:47:58 +0000

    fix return sequence mismatch

--- run_phi3.py
+++ run_phi3.py
@@ -96,10 +96,12 @@ def run_hf_pipeline(prompt: str, pretrained_model_path: str, tokenizer_model_pat
     model = AutoModelForCausalLM.from_pretrained(
         pretrained_model_path,
         device_map="cuda",
-        torch_dtype="auto",
+        torch_dtype=torch.float16,
         trust_remote_code=True,
         output_hidden_states=True,
-        return_dict_in_generate=True
+        return_dict_in_generate=True,
+        attn_implementation="flash_attention_2",
+        eos_token_id=[32000,32001,32007]
     )
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_path)
     
